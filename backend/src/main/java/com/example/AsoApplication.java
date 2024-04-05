aae279d854b328af81419df7a22211f78eeff616
Kuba Drałus <kubadralus@gmail.com>
2024-04-05 16:55:25 +0200

    database connection

--- backend/src/main/java/com/example/AsoApplication.java
+++ backend/src/main/java/com/example/AsoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AsoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AsoApplication.class, args);
 	}
