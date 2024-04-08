1dd433d90828d0ae49e330eb73ab5e7f852155b9
Kuba Drałus <kubadralus@gmail.com>
2024-04-08 14:59:34 +0200

    first db migration

---
+++ backend/src/main/resources/db/migration/V1.001__init.sql
@@ -0,0 +1,22 @@
+create table address (
+    id bigserial not null,
+    country varchar(255),
+    city varchar(255),
+    street varchar(255),
+    postal_code varchar(255),
+    primary key (id)
+);
+
+CREATE TABLE user_data (
+    id bigserial not null,
+    first_name varchar(255),
+    last_name varchar(255),
+    telephone_number varchar(255),
+    email varchar(255),
+    password varchar(255),
+    role varchar(255),
+    created_at timestamp(6),
+    address_id bigint,
+    primary key (id),
+    foreign key (address_id) references address(id)
+);
