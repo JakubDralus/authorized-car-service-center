016ab94e87c826a3f2d97c26c7d4bdda6e13066e
lukiiis <lukaszczyszczon01@gmail.com>
2024-04-14 23:16:33 +0200

    ASO-61 made home banner (maybe change to slider in the future), changed styles

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,10 +1,28 @@
 import Navbar from "../../components/navbar/Navbar"
+import "./Home.css"
+import { Link } from "react-router-dom"
 
 const Home = () =>{
 
 
     return (
-        <Navbar/>
+        <>
+            <Navbar/>
+            <div className="home-container">
+                <div className="home-banner">
+                    <div className="banner-wrapper">
+                        <div className="banner-info-container">
+                            <h1>Schedule a service appointment</h1>
+                            <div>Servicing your Škoda can be really fun!</div>
+                            <Link to="/">Schedule an appointment</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="home-slogan">
+
+                </div>
+            </div>
+        </>
     )
 }
 export default Home
