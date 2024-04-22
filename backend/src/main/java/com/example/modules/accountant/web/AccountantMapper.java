c83f291a807c4785b27d0d457bdf3a85c2f8d4d2
Filip Jędrzejek <veczarovsky@gmail.com>
2024-04-22 14:58:39 +0200

    ASO-76 Accountant Mapper

---
+++ backend/src/main/java/com/example/modules/accountant/web/AccountantMapper.java
@@ -0,0 +1,36 @@
+package com.example.modules.accountant.web;
+
+import com.example.modules.accountant.Accountant;
+import com.example.modules.user.User;
+import com.example.modules.user.UserMapper;
+import com.example.modules.user.UserRepository;
+import com.example.shared.IMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AccountantMapper implements IMapper<Accountant,AccountantDTO> {
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    @Override
+    public AccountantDTO toDto(Accountant accountant) {
+        return AccountantDTO.builder()
+                .accountantId(accountant.getAccountantId())
+                .salary(accountant.getSalary())
+                .user(userMapper.toDto(accountant.getUser()))
+                .build();
+    }
+
+    @Override
+    public void toEntity(AccountantDTO accountantDTO, Accountant accountant) {
+        accountant.setSalary(accountantDTO.getSalary());
+        if(accountantDTO.getUser() != null) setUser(accountantDTO,accountant);
+    }
+
+    private void setUser(AccountantDTO accountantDTO, Accountant accountant) {
+        User user = userRepository.findById(accountantDTO.getUser().getUserId()).orElseThrow();
+        accountant.setUser(user);
+    }
+}
