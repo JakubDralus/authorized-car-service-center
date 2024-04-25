ab8bd8cdb5396bdeadd639eeebc28706fbed4f46
Filip Jędrzejek <veczarovsky@gmail.com>
2024-04-25 15:19:30 +0200

    ASO-76 clean code

--- backend/src/main/java/com/example/modules/accountant/AccountantService.java
+++ backend/src/main/java/com/example/modules/accountant/AccountantService.java
@@ -15,32 +15,29 @@ public class AccountantService implements CrudService<AccountantDTO> {
     private final AccountantRepository accountantRepository;
     private final AccountantMapper accountantMapper;
 
-    public List<AccountantDTO> getAll(){
+    public List<AccountantDTO> getAll() {
         return accountantRepository.findAll().stream().map(accountantMapper::toDto).toList();
     }
     @Override
-    public AccountantDTO get(Long id){
+    public AccountantDTO get(Long id) {
         return accountantMapper.toDto(accountantRepository.findById(id).orElseThrow());
     }
-
     @Override
-    public AccountantDTO create(AccountantDTO accountantDTO){
+    public AccountantDTO create(AccountantDTO accountantDTO) {
         Accountant accountant = new Accountant();
         accountantMapper.toEntity(accountantDTO,accountant);
         accountant = accountantRepository.save(accountant);
         return accountantMapper.toDto(accountant);
     }
-
     @Override
-    public AccountantDTO update(AccountantDTO accountantDTO){
+    public AccountantDTO update(AccountantDTO accountantDTO) {
         Accountant accountant = accountantRepository.findById(accountantDTO.getAccountantId()).orElseThrow();
         accountantMapper.toEntity(accountantDTO,accountant);
         accountant = accountantRepository.save(accountant);
         return accountantMapper.toDto(accountant);
     }
-
     @Override
-    public void delete(Long id){
+    public void delete(Long id) {
         if(accountantRepository.existsById(id)) accountantRepository.deleteById(id);
         else throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't find the accountant.");
     }
