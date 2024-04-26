dc0d6b107964f566332443b317bb8eb925360ce5
Kuba Drałus <kubadralus@gmail.com>
2024-04-26 13:42:35 +0200

    fix AccountantController response type

--- backend/src/main/java/com/example/modules/accountant/web/AccountantController.java
+++ backend/src/main/java/com/example/modules/accountant/web/AccountantController.java
@@ -1,17 +1,15 @@
 package com.example.modules.accountant.web;
 
 import com.example.modules.accountant.AccountantService;
-import com.example.shared.ApiHttpResponse;
+import com.example.shared.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/accountants")
 @RequiredArgsConstructor
+@RequestMapping("api/v1/accountants")
 public class AccountantController {
     private final AccountantService accountantService;
 
@@ -36,12 +34,10 @@ public class AccountantController {
     }
 
     @DeleteMapping("/{accountantId}")
-    public ResponseEntity<ApiHttpResponse> delete(@PathVariable Long accountantId){
+    public ApiResponse<?> delete(@PathVariable Long accountantId){
         accountantService.delete(accountantId);
-        return ResponseEntity.ok().body(ApiHttpResponse.builder()
-                .timeStamp(LocalDateTime.now().toString())
+        return ApiResponse.builder()
                 .message("Accounant deleted")
-                .build());
+                .build();
     }
-
 }
