7f57b043bfee8dfae01ba42e2cc068c04ebcf0ea
Kuba Drałus <kubadralus@gmail.com>
2024-05-02 18:19:09 +0200

    fix indentation

--- backend/src/main/resources/db/migration/V1.010__review.sql
+++ backend/src/main/resources/db/migration/V1.010__review.sql
@@ -1,11 +1,11 @@
 CREATE TABLE review (
-review_id BIGSERIAL NOT NULL,
-title VARCHAR(255),
-description VARCHAR(255),
-created_at TIMESTAMP,
-user_id BIGINT,
-PRIMARY KEY (review_id),
-FOREIGN KEY (user_id)
-    REFERENCES user_data(user_id)
-    ON DELETE CASCADE
+    review_id BIGSERIAL NOT NULL,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    created_at TIMESTAMP,
+    user_id BIGINT,
+    PRIMARY KEY (review_id),
+    FOREIGN KEY (user_id)
+        REFERENCES user_data(user_id)
+        ON DELETE CASCADE
 );
