fc509d7e3dcb210e0992129fcaa2ea9da737ed35
Filip Jędrzejek <veczarovsky@gmail.com>
2024-05-07 16:29:30 +0200

    ASO-77 Invoice controller

---
+++ backend/src/main/java/com/example/modules/invoice/web/InvoiceController.java
@@ -0,0 +1,56 @@
+package com.example.modules.invoice.web;
+
+import com.example.modules.invoice.Invoice;
+import com.example.modules.invoice.InvoiceService;
+import com.example.shared.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/invoices")
+public class InvoiceController {
+    private final InvoiceService invoiceService;
+
+    @GetMapping
+    public List<InvoiceDTO> getAll(){
+        return invoiceService.getAll();
+    }
+
+    @GetMapping("/{invoiceId}")
+    public ApiResponse<InvoiceDTO> get(@PathVariable Long invoiceId){
+        InvoiceDTO invoiceDTO = invoiceService.get(invoiceId);
+        return ApiResponse.<InvoiceDTO>builder()
+                .message("Invoice Fetched")
+                .data(invoiceDTO)
+                .build();
+    }
+
+    @PostMapping
+    public ApiResponse<InvoiceDTO> create(@RequestBody InvoiceDTO invoiceDTO){
+        InvoiceDTO created = invoiceService.create(invoiceDTO);
+        return ApiResponse.<InvoiceDTO>builder()
+                .message("Invoice created")
+                .data(created)
+                .build();
+    }
+
+    @PutMapping
+    public ApiResponse<InvoiceDTO> update(@RequestBody InvoiceDTO invoiceDTO){
+        InvoiceDTO updated = invoiceService.create(invoiceDTO);
+        return ApiResponse.<InvoiceDTO>builder()
+                .message("Invoice updated")
+                .data(updated)
+                .build();
+    }
+    @DeleteMapping("/{invoiceId}")
+    public ApiResponse<InvoiceDTO> delete(@PathVariable Long invoiceId){
+        invoiceService.delete(invoiceId);
+        return ApiResponse.<InvoiceDTO>builder()
+                .message("Invoice deleted")
+                .build();
+    }
+
+}
