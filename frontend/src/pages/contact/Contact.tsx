f26f19acc3d73e874d28591b3a5c49f6cb62a1df
Filip Jędrzejek <veczarovsky@gmail.com>
2024-05-08 18:37:32 +0200

    Contact base page

---
+++ frontend/src/pages/contact/Contact.tsx
@@ -0,0 +1,160 @@
+import Navbar from "../../components/navbar/Navbar"
+import Footer from "../../components/footer/Footer"
+import "./Contact.css"
+
+const Contact = () => {
+    return (
+    <> 
+        <Navbar/>
+           <div className="contact-container">
+                <div className="contact-banner">
+                    <div className="banner-wrapper">
+                        <div className="banner-info-container">
+                            <h1>Contact us</h1>
+                            <div>Get in touch with us</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="content-wrap">
+                    <div className="contact-info">
+                        <div className="contact-info-wrap">
+                          <div className="info"> 
+                            <div className="info-col">
+                                <div className="info-box-col"> 
+                                  <div className="info-box-col-content">
+                                    <div className="info-box">
+                                      <div className="info-box--left-right-wrap">
+                                        <div className="info-box-content">
+                                            <div className="info-box-content-icon">
+
+                                            </div>
+                                            <div className="info-box-content-title">
+                                                <h3>Opening Times </h3>
+                                            </div>
+
+                                            <div className="info-box-separator">
+                                                <div className="info-box-separator-line">
+                                                </div>
+                                            </div>
+                                            
+                                            <div className="info-box-content-text">
+                                                <div className="info-box-text">
+                                                    <p>Monday - Friday 24/7</p> 
+                                                </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+                                      
+                                  </div> 
+                                </div>
+                            </div>
+                          </div>
+
+                          <div className="info"> 
+                            <div className="info-col">
+                                <div className="info-box-col"> 
+                                  <div className="info-box-col-content">
+                                    <div className="info-box">
+                                      <div className="info-box--left-right-wrap">
+                                        <div className="info-box-content">
+                                            <div className="info-box-content-icon">
+
+                                            </div>
+                                            <div className="info-box-content-title">
+                                                <h3>Contact Phone</h3>
+                                            </div>
+
+                                            <div className="info-box-separator">
+                                                <div className="info-box-separator-line">
+                                                </div>
+                                            </div>
+                                            
+                                            <div className="info-box-content-text">
+                                                <div className="info-box-text">
+                                                    <p>669 244 105</p> 
+                                                </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+                                      
+                                  </div> 
+                                </div>
+                            </div>
+                          </div>
+
+                          <div className="info"> 
+                            <div className="info-col">
+                                <div className="info-box-col"> 
+                                  <div className="info-box-col-content">
+                                    <div className="info-box">
+                                      <div className="info-box--left-right-wrap">
+                                        <div className="info-box-content">
+                                            <div className="info-box-content-icon">
+
+                                            </div>
+                                            <div className="info-box-content-title">
+                                                <h3>E-mail </h3>
+                                            </div>
+
+                                            <div className="info-box-separator">
+                                                <div className="info-box-separator-line">
+                                                </div>
+                                            </div>
+                                            
+                                            <div className="info-box-content-text">
+                                                <div className="info-box-text">
+                                                    <p>krolstarejwsi69@gmail.com</p> 
+                                                </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+                                      
+                                  </div> 
+                                </div>
+                            </div>
+                          </div>
+                          
+                          <div className="info"> 
+                            <div className="info-col">
+                                <div className="info-box-col"> 
+                                  <div className="info-box-col-content">
+                                    <div className="info-box">
+                                      <div className="info-box--left-right-wrap">
+                                        <div className="info-box-content">
+                                            <div className="info-box-content-icon">
+
+                                            </div>
+                                            <div className="info-box-content-title">
+                                                <h3>Adress </h3>
+                                            </div>
+
+                                            <div className="info-box-separator">
+                                                <div className="info-box-separator-line">
+                                                </div>
+                                            </div>
+                                            
+                                            <div className="info-box-content-text">
+                                                <div className="info-box-text">
+                                                    <p>Mieczysława Medweckiego 2, 31-870 Kraków</p> 
+                                                </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+                                      
+                                  </div> 
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        <Footer/>
+    </>)
+}
+export default Contact
