08e0d4f61a083b1f2bcff60d5d215c5d08288592
Kuba Drałus <kubadralus@gmail.com>
2024-05-08 16:12:06 +0200

    ASO-82 fixed double user fetching and response DTO

--- backend/src/main/java/com/example/modules/auth/web/AuthResponseDTO.java
+++ backend/src/main/java/com/example/modules/auth/web/AuthResponseDTO.java
@@ -12,9 +12,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthResponseDTO {
     private String token;
-    private String role;
     private String expirationDate;
+    private Long id;
     private String firstName;
     private String lastName;
-    private Long id;
+    private String role;
 }
