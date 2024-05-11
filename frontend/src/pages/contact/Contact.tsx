2a6befec0d9a383b859d09e17f4362b662f929b3
Filip Jędrzejek <veczarovsky@gmail.com>
2024-05-11 13:31:03 +0200

    ASO-83 google map + some css changes

--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -1,8 +1,20 @@
 import Navbar from "../../components/navbar/Navbar"
 import Footer from "../../components/footer/Footer"
+import React, { useState } from 'react';
 import "./Contact.css"
 
 const Contact = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const infoClassName = `info${isHovered ? '.hovered' : ''}`;
     return (
     <> 
         <Navbar/>
@@ -19,7 +31,7 @@ const Contact = () => {
                 <div className="content-wrap">
                     <div className="contact-info">
                         <div className="contact-info-wrap">
-                          <div className="info"> 
+                          <div className="info" > 
                             <div className="info-col">
                                 <div className="info-box-col"> 
                                   <div className="info-box-col-content">
@@ -157,11 +169,21 @@ const Contact = () => {
                 <div className="form-row-content-wrap">
                   <div className="form-row-content">
                     <div className="form-col-group">
-                      <div className="form-col"></div>
+                      <div className="form-col">
+                        <div className="form-map-col">
+                          <div className="form-map-col-module">
+                            <div className="form-map-col-content">
+                              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2560.5038975127472!2d20.014752812340166!3d50.07685191407425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716453af1fd622f%3A0xa62689a1c5160b1f!2zS0ZDIEtyYWvDs3cgQ3p5xbx5bnk!5e0!3m2!1spl!2spl!4v1715425964801!5m2!1spl!2spl" width="600" height="450" className="map"></iframe>   
+                            </div>
+                          </div>
+                        </div>
+                        
+                      </div>
 
                       <div className="form-col">
                         <div className="form-col-content">
                           <div className="form-col-header">
+                            
                             <h2>
                               <span> Message us</span>
                             </h2>
