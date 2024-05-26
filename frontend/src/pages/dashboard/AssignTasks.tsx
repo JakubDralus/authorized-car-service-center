1134d87f3d81d004d8d2cec5a1b88511dcc9d3df
Kuba Drałus <kubadralus@gmail.com>
2024-05-26 15:23:19 +0200

    ASO-90 util functions refactor

--- frontend/src/pages/dashboard/AssignTasks.tsx
+++ frontend/src/pages/dashboard/AssignTasks.tsx
@@ -3,9 +3,8 @@ import { DragDropContext, DropResult } from "@hello-pangea/dnd";
 import Dialog from "../../components/dashboard_components/Dialog";
 import { ApiResponse, AssignmentRead, Mechanic, ServiceRead, Task, TicketRead } from "../../api/model";
 import TaskColumn from "../../components/dashboard_components/TaskColumn";
-import axios from "axios";
 import { useQuery } from "react-query";
-import { log } from "console";
+import { createAssignment, fetchAssignments, fetchMechanics, fetchTickets, updateAssignment, updateTicketStatus } from "./TaskBoardFunctions";
 
 // Define types for column and task
 export type TaskColumnType = {
@@ -14,82 +13,6 @@ export type TaskColumnType = {
   mechanic?: Mechanic;
   tasks: any[]; // Task[]
 }
-
-// Create an assignment/task for a mechanic
-const createAssignment = async (task: Task) => {
-  const payload = {
-    description: task.description,
-    startTime: task.startTime?.toISOString(),
-    endTime: task.endTime?.toISOString(),
-    ticket: { ticketId: task.ticket?.ticketId },
-    mechanic: { mechanicId: task.mechanic?.mechanicId },
-    manager: { managerId: task.manager?.managerId ?? 1 }, //get manager id from logged manager
-    service: { serviceId: task.service?.serviceId }
-  };
-  console.log(payload);
-  
-  try {
-    const response = await axios.post("http://localhost:8081/api/v1/assignments", payload);
-    console.log(response.data);
-    return response;
-  } 
-  catch (error) {
-    console.error('Error creating assignment:', error);
-  }
-};
-
-// Function to update an existing assignment
-const updateAssignment = async (task: Task) => {
-  const payload = {
-    description: task.description,
-    startTime: task.startTime?.toISOString(),
-    endTime: task.endTime?.toISOString(),
-    ticket: { ticketId: task.ticket?.ticketId },
-    mechanic: { mechanicId: task.mechanic?.mechanicId },
-    manager: { managerId: task.manager?.managerId ?? 1 }, //get manager id from logged manager
-    service: { serviceId: task.service?.serviceId }
-  };
-  // console.log(payload);
-
-  try {
-    await axios.put(`http://localhost:8081/api/v1/assignments/${task.id}`, payload);
-    // console.log('Assignment updated:', response.data);
-    console.log('Assignment updated');
-  } catch (error) {
-    console.error('Error updating assignment:', error);
-    throw error;
-  }
-};
-
-// Function to update ticket status
-const updateTicketStatus = async (ticketId: number, status: string) => {
-  try {
-    const response: ApiResponse<any> = await axios.put(`http://localhost:8081/api/v1/tickets/${ticketId}`, { status });
-    console.log(`Ticket ${ticketId} status updated to ${status}`);
-    console.log(response);
-  } 
-  catch (error) {
-    console.error(`Error updating ticket ${ticketId} status:`, error);
-  }
-};
-
-const fetchTickets = async (): Promise<ApiResponse<TicketRead[]>> => {
-  const { data } = await axios.get<ApiResponse<TicketRead[]>>( "http://localhost:8081/api/v1/tickets/requested");
-  console.log('ticket fetch');
-  return data;
-};
-
-const fetchMechanics = async (): Promise<ApiResponse<Mechanic[]>> => {
-  const { data } = await axios.get<ApiResponse<Mechanic[]>>( "http://localhost:8081/api/v1/mechanics");
-  console.log('mechanics fetch');
-  return data;
-};
-
-const fetchAssignments = async (): Promise<ApiResponse<AssignmentRead[]>> => {
-  const { data } = await axios.get<ApiResponse<AssignmentRead[]>>(`http://localhost:8081/api/v1/assignments`);
-  console.log('assignments fetch');
-  return data;
-};
 
 const AssignTasks = () => {
   
@@ -241,7 +164,6 @@ const AssignTasks = () => {
             
             return assignment;
           });
-
         
         // Wait for all assignments to be created
         await Promise.all(createAssignmentPromises);
