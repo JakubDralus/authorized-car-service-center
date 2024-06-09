f73e5c7b64065919fd02d92b248cc746a80cceb0
lukiiis <lukaszczyszczon01@gmail.com>
2024-06-09 16:13:48 +0200

    ASO-107 homepage done

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -6,44 +6,42 @@ import ServiceCarousel from "../../components/service_carousel/ServiceCarousel"
 import { fetchFeaturedServices } from '../service/serviceData';
 import { useQuery } from 'react-query';
 
-const Home = () =>{
-    const {data, error, isLoading} = useQuery(['featuredServices'], fetchFeaturedServices);
-    
+const Home = () => {
+    const { data, isLoading } = useQuery(['featuredServices'], fetchFeaturedServices);
+
     return (
         <>
             <Navbar />
-
-            <div className="home-container">
+            <div className="w-full h-full">
                 <div className="home-banner">
-                    <div className="banner-wrapper">
-                        <div className="banner-info-container">
-                            <h1>Schedule a service appointment</h1>
-                            <div>Servicing your ....... can be really fun!</div>
-                            <Link className="home-button" to="/">Schedule an appointment</Link>
+                    <div className="max-w-6xl my-0 mx-auto h-full">
+                        <div className="flex flex-col items-start justify-center h-full gap-16">
+                            <div className="flex flex-col gap-6">
+                                <h1 className="text-5xl font-semibold text-black tracking-wide">Schedule a service appointment</h1>
+                                <span className="text-2xl text-black">Servicing your car can be really fun!</span>
+                            </div>
+                            <Link className="no-underline border-solid border border-white text-white rounded-3xl transition-all py-3 px-5 bg-slate-500 text-lg hover:bg-slate-600" to="/">Create new ticket</Link>
                         </div>
                     </div>
                 </div>
-                <div className="home-slogan">
-                    <div className="home-wrapper">
-                        <h1>Our service is a guarantee of quality!</h1>
-                        <span>........ Service offers you easy and fast solutions, tailored to your needs. Save time and have peace of mind. Servicing your ........ can really be a pleasure!</span>
+                <div className="h-64">
+                    <div className="max-w-7xl my-0 mx-auto h-full flex items-center justify-center flex-col gap-8">
+                        <h1 className="text-4xl text-center">Our service is a guarantee of quality!</h1>
+                        <span className="text-lg text-center max-w-3xl"> Our service offers you easy and fast solutions, tailored to your needs. Save time and have peace of mind. Servicing your car can really be a pleasure!</span>
                     </div>
                 </div>
                 <div className="home-example-services">
                     {isLoading ? (<div className="pt-10"><div className="spinner "></div></div>) : (<ServiceCarousel serviceData={data} />)}
                 </div>
-
-                {/* maybe something more, we'll see */}
-
-                <div className="home-schedule">
-                    <div className="home-schedule-info">
-                        <h1>Book a service appointment</h1>
-                        <span>Let the specialists take care of your car.</span>
-                        <Link className="home-button" to="/ticket-form">Create a ticket</Link>
+                <div className="home-schedule h-96 flex items-center justify-center">
+                    <div className="max-w-xl flex flex-col items-center justify-center gap-8 text-white">
+                        <h1 className="text-4xl">Book a service appointment</h1>
+                        <span className="text-xl">Let the specialists take care of your car.</span>
+                        <Link className="no-underline border-solid border border-slate-300 text-white rounded-3xl transition-all py-3 px-5 bg-violet-700 text-lg hover:bg-violet-800" to="/ticket-form">Create a ticket</Link>
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
